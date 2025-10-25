29640cdb25a45bd1c4caa269a4529670df718e4b
Aron Ledesma <ledearo2027@gmail.com>
2025-10-25 11:26:24 -0500

    font-end

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -9,7 +9,7 @@ const [response, setResponse] = useState("");
 
 const handleInput = async () => {
     if (prompt == ""){
-        setResponse("where's the prompt bruh");
+        setResponse("Enter a prompt to continue.");
     } else {
         setResponse(prompt)
     }
@@ -17,21 +17,25 @@ const handleInput = async () => {
 };
 
     return (
-        <div className="bg-red-500 bg-cover bg-fixed h-screen text-white">
-                        {/* bg-[url(/file-name.jpg)] */}
-            <div className="bg-black/90 h-screen content-center">
-                <div className="max-w-[1140px] mx-auto items-center text-center flex flex-col">
-                    <h1>h1 title</h1>
-                    <textarea 
-                        onChange={(e) => setPrompt(e.target.value)}
-                        value={prompt}
-                        placeholder="input something..."
-                        className="w-1/2 h-[10rem] border-1 bg-gray-200 text-black rounded-lg p-1"
-                    />
-                    <button onClick={handleInput} className="w-1/2 px-5 py-2 rounded-lg bg-blue-400 hover:bg-blue-500">button</button>
-                    <p>{response}</p>
-                </div>
-            </div>
+        <div
+      className="bg-cover bg-fixed h-screen text-white"
+      style={{ backgroundImage: `url(${"https://media.nbcnewyork.com/2025/10/42926846218-1080pnbcstations.jpg?quality=85&strip=all"})` }}
+    >
+      <div className="bg-black/90 h-screen flex items-center justify-center">
+        <div className="max-w-[1140px] w-full mx-auto text-center flex flex-col items-center gap-y-4">
+          <h1 className="text-7xl font-bold mb-3">SwishStats</h1>
+          <textarea
+            onChange={(e) => setPrompt(e.target.value)}
+            value={prompt}
+            placeholder="Ex: Who led the league in blocked shots?"
+            className="w-1/2 h-[10rem] border-1 bg-gray-200/90 text-black rounded-lg p-2 focus:ring-2 focus:ring-blue-400 focus:outline-none"
+          />
+          <button
+            onClick={handleInput}
+            className="w-1/2 px-5 py-3 rounded-lg bg-blue-500 hover:bg-blue-600 font-bold transition-colors">Submit
+          </button>
         </div>
+      </div>
+    </div>
     );
 }
